14ba0f643bd27367756076074ad1d14897d02acb
Mawichoo <cristian_otalora@ingenieros.com>
2022-02-23 15:37:27 -0500

    migrations done

---
+++ database/migrations/2022_02_23_164457_create_candidates_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCandidatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->text('principal');
+            $table->text('principal_faculty');
+            $table->text('principal_program');
+            $table->text('candidate');
+            $table->text('candidate_faculty');
+            $table->text('candidate_program');
+            $table->foreignId('voting_option_id')->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+}
